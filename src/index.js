23124cb9271772ce68ad836716f668402893427a
Anna Kravets <annakravets1906@gmail.com>
2023-04-01 12:08:42 +0300

    Додано галерею

--- src/index.js
+++ src/index.js
@@ -1,25 +1,62 @@
 import { FetchImagesAPI } from './js/formSearch';
 import Notiflix from 'notiflix';
 
-const formEl = document.querySelector('.search-form');
-
-formEl.addEventListener('submit', onSubmitClick);
-
+const refs = {
+  formEl: document.querySelector('.search-form'),
+  listEl: document.querySelector('.gallery'),
+};
 const fetchImagesAPI = new FetchImagesAPI();
+
+refs.formEl.addEventListener('submit', onSubmitClick);
 
 function onSubmitClick(event) {
   event.preventDefault();
-  fetchImagesAPI.query = formEl.elements.searchQuery.value.trim();
-  console.log(formEl.elements.searchQuery.value);
+  fetchImagesAPI.query = refs.formEl.elements.searchQuery.value.trim();
+  console.log(refs.formEl.elements.searchQuery.value);
+
+  function clearMarkUp() {
+    refs.listEl.innerHTML = '';
+  }
 
   fetchImagesAPI
     .fetchImages()
     .then(data => {
-      console.log(data);
+      if (data.total === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      if (data.total !== 0) {
+        markuplist(data);
+      }
     })
+
     .catch(error => {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
+      console.log(error);
     });
+}
+
+function markuplist(el) {
+  const imagesList = el.hits.map(
+    element =>
+      `<div class="photo-card">
+        <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+          <b>Likes</b> ${element.likes}
+          </p>
+          <p class="info-item">
+          <b>Views</b> ${element.views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b> ${element.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b> ${element.downloads}
+          </p>   
+        </div>
+      </div>`
+  );
+
+  refs.listEl.innerHTML = imagesList;
 }
--- src/js/formSearch.js
+++ src/js/formSearch.js
@@ -2,9 +2,12 @@ export class FetchImagesAPI {
   #BASE_URL = 'https://pixabay.com/api/';
   #API_KEY = '34753200-909a3cccc831787159f9f5943';
 
-  fetchImages(query) {
+  // options {
+
+  // }
+  fetchImages() {
     return fetch(
-      `${this.#BASE_URL}?${this.#API_KEY}&q=${
+      `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
         this.query
       }&image_type=photo&orientation=horizontal&safesearch=true`
     ).then(res => {
