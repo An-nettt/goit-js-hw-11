19bcb378a749cd8e95c041dc3815af9757eba825
Anna Kravets <annakravets1906@gmail.com>
2023-04-02 11:17:41 +0300

    Додано css

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
+'use strict';
+
 import { FetchImagesAPI } from './js/formSearch';
 import Notiflix from 'notiflix';
 
 const refs = {
   formEl: document.querySelector('.search-form'),
   listEl: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
 const fetchImagesAPI = new FetchImagesAPI();
+let imagesList = '';
+refs.loadMoreBtn.classList.add('is-hidden');
 
 refs.formEl.addEventListener('submit', onSubmitClick);
+refs.loadMoreBtn.addEventListener('click', loadMoreBtnClick);
 
 function onSubmitClick(event) {
   event.preventDefault();
   fetchImagesAPI.query = refs.formEl.elements.searchQuery.value.trim();
-  console.log(refs.formEl.elements.searchQuery.value);
 
-  function clearMarkUp() {
-    refs.listEl.innerHTML = '';
-  }
+  clearMarkUp();
+  fetchImagesAPI.page = 1;
 
   fetchImagesAPI
     .fetchImages()
     .then(data => {
+      console.log(data);
       if (data.total === 0) {
         Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
@@ -28,16 +33,20 @@ function onSubmitClick(event) {
       }
       if (data.total !== 0) {
         markuplist(data);
+        refs.listEl.innerHTML = imagesList;
+
+        // if (data.totalHits === fetchImagesAPI.page) {
+        // }
+        refs.loadMoreBtn.classList.remove('is-hidden');
       }
     })
-
     .catch(error => {
       console.log(error);
     });
 }
 
 function markuplist(el) {
-  const imagesList = el.hits.map(
+  imagesList = el.hits.map(
     element =>
       `<div class="photo-card">
         <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
@@ -57,6 +66,19 @@ function markuplist(el) {
         </div>
       </div>`
   );
+  return imagesList;
+}
 
-  refs.listEl.innerHTML = imagesList;
+function clearMarkUp() {
+  refs.listEl.innerHTML = '';
+}
+
+function loadMoreBtnClick() {
+  console.log(fetchImagesAPI.page);
+  fetchImagesAPI.page += 1;
+  fetchImagesAPI.fetchImages().then(data => {
+    // console.log(data);
+    markuplist(data);
+    refs.listEl.insertAdjacentHTML('beforeend', imagesList);
+  });
 }
--- src/js/formSearch.js
+++ src/js/formSearch.js
@@ -2,15 +2,27 @@ export class FetchImagesAPI {
   #BASE_URL = 'https://pixabay.com/api/';
   #API_KEY = '34753200-909a3cccc831787159f9f5943';
 
-  // options {
+  query = null;
+  page = 1;
+  count = 40;
 
-  // }
+  options = {
+    key: this.#API_KEY,
+    per_page: this.count,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
   fetchImages() {
-    return fetch(
-      `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
-        this.query
-      }&image_type=photo&orientation=horizontal&safesearch=true`
-    ).then(res => {
+    const searchParams = new URLSearchParams({
+      q: this.query,
+      page: this.page,
+      ...this.options,
+    });
+    console.log(`${searchParams}`);
+
+    return fetch(`${this.#BASE_URL}?${searchParams}`).then(res => {
       if (!res.ok) {
         throw new Error(res.status);
       }
